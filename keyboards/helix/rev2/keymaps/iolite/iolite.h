48a3a7e2043ffba01a168fc5be6e8c777244b03d
yamaimo <yarnaimo@gmail.com>
2020-01-06 02:33:52 +0900

    remove IK_

--- keyboards/helix/rev2/keymaps/iolite/iolite.h
+++ keyboards/helix/rev2/keymaps/iolite/iolite.h
@@ -4,11 +4,6 @@
 #define SS_PAIR(str) str SS_TAP(X_LEFT)
 
 typedef enum iolite_keycodes {
-  IL_W,
-  IL_R,
-  IL_X,
-  IL_V,
-
   SP_RAIS = SAFE_RANGE,
   ZH_RAIS,
   // IL_HOME,
@@ -30,30 +25,30 @@ typedef enum iolite_keycodes {
 
 // #define IL_SAFE_RANGE SAFE_RANGE+63
 
-#define IL_Q    KC_Q
-// #define IL_W    KC_SCLN // ei
+#define IL_Q    KC_X
+#define IL_W    KC_E
 #define IL_E    KC_A
-// #define IL_R    KC_COMM // ai
+#define IL_R    JP_MHEN
 #define IL_T    KC_J
 #define IL_Y    KC_F
 #define IL_U    KC_G
 #define IL_I    KC_K
 #define IL_O    KC_H
 #define IL_P    KC_B
-#define IL_A    KC_L
-#define IL_S    KC_E
+#define IL_A    KC_C
+#define IL_S    KC_O
 #define IL_D    KC_U
 #define IL_F    KC_I
-#define IL_G    KC_C
+#define IL_G    KC_L
 #define IL_H    KC_D
 #define IL_J    KC_T
 #define IL_K    KC_S
 #define IL_L    KC_N
 #define IL_SCLN KC_Z
-#define IL_Z    KC_X
-// #define IL_X    KC_DOT // ou
-#define IL_C    KC_O
-// #define IL_V    KC_SLSH // uu
+#define IL_Z    KC_Q
+#define IL_X    JP_EQL
+#define IL_C    KC_SLSH
+#define IL_V    KC_MINS
 #define IL_B    KC_V
 #define IL_N    KC_Y
 #define IL_M    KC_M
--- keyboards/helix/rev2/keymaps/iolite/keymap.c
+++ keyboards/helix/rev2/keymaps/iolite/keymap.c
@@ -50,22 +50,27 @@ enum keymap_layers {
 #define C_X LCTL(KC_X)
 #define C_C LCTL(KC_C)
 #define C_V LCTL(KC_V)
+#define C_SLSH LCTL(KC_SLSH)
 #define CS_Z LCTL(LSFT(KC_Z))
+#define CS_I LCTL(LSFT(KC_I))
+#define MOZH_MH LT(JP_MHEN)
+#define S_COLN SFT_T(JP_COLN)
+#define A_ZHTG ALT_T(JP_ZHTG)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_IOLITE] = LAYOUT(
     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,  \
     KC_TAB,  IL_Q,    IL_W,    IL_E,    IL_R,    IL_T,                      IL_Y,    IL_U,    IL_I,    IL_O,    IL_P,    KC_BSPC, \
-    KC_LALT, IL_A,    IL_S,    IL_D,    IL_F,    IL_G,                      IL_H,    IL_J,    IL_K,    IL_L,    IL_SCLN, KC_ENT,  \
-    KC_LSFT, IL_Z,    IL_X,    IL_C,    IL_V,    IL_B,    C_C,     C_Z,     IL_N,    IL_M,    IL_COMM, IL_DOT,  KC_UP,   KC_MINS, \
-    KC_LCTL, KC_LGUI, _______, KC_CAPS, JP_MHEN, KC_COMM, ZH_RAIS, SP_RAIS, KC_DOT,  IL_SLSH, JP_HENK, KC_LEFT, KC_DOWN, KC_RGHT  \
+    A_ZHTG,  IL_A,    IL_S,    IL_D,    IL_F,    IL_G,                      IL_H,    IL_J,    IL_K,    IL_L,    IL_SCLN, KC_ENT,  \
+    KC_LSFT, IL_Z,    IL_X,    IL_C,    IL_V,    IL_B,    C_C,     C_Z,     IL_N,    IL_M,    IL_COMM, IL_DOT,  KC_UP,   JP_COLN,  \
+    KC_LCTL, KC_LGUI, _______, KC_CAPS, JP_MHEN, KC_COMM, SP_RAIS, SP_RAIS, KC_DOT,  IL_SLSH, JP_HENK, KC_LEFT, KC_DOWN, KC_RGHT  \
   ),
 
   [_RAISE] = LAYOUT(
     _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_PSCR, KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS, _______, \
     _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,                    KC_MUTE, KC_P7,   KC_P8,   KC_P9,   KC_PPLS, _______, \
-    _______, KC_F11,  KC_F12,  _______, _______, _______,                   KC_PAUS, KC_P4,   KC_P5,   KC_P6,   KC_PENT, _______, \
-    _______, _______, _______, _______, _______, C_X,     C_V,     _______, KC_INS,  KC_P1,   KC_P2,   KC_P3,   KC_PDOT, _______, \
+    _______, KC_F11,  KC_F12,  _______, CS_I,    C_SLSH,                    KC_PAUS, KC_P4,   KC_P5,   KC_P6,   KC_PENT, _______, \
+    _______, _______, _______, _______, _______, C_X,     C_V,     CS_Z,    KC_INS,  KC_P1,   KC_P2,   KC_P3,   KC_PDOT, _______, \
     _______, _______, _______, RESET,   AG_SWAP, AG_NORM, _______, _______, KC_P0,   KC_P0,   _______, _______, _______, _______  \
   ),
 };
@@ -226,34 +231,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         if (is_tapped) {
           tap_code(KC_SPC);
         }
-      }
-      return false;
-      break;
-
-    case IL_W:
-      if (record->event.pressed) {
-        SEND_STRING("ei");
-      }
-      return false;
-      break;
-
-    case IL_R:
-      if (record->event.pressed) {
-        SEND_STRING("ai");
-      }
-      return false;
-      break;
-
-    case IL_X:
-      if (record->event.pressed) {
-        SEND_STRING("ou");
-      }
-      return false;
-      break;
-
-    case IL_V:
-      if (record->event.pressed) {
-        SEND_STRING("uu");
       }
       return false;
       break;
