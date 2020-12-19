7c88b86367be90c9b7237545c04e334276ef19e2
yamaimo <yarnaimo@gmail.com>
2020-12-19 19:13:37 +0900

    like lily58

--- keyboards/helix/rev2/keymaps/iolite/combo.c
+++ keyboards/helix/rev2/keymaps/iolite/combo.c
@@ -4,10 +4,10 @@
 #include "combo.h"
 
 const PROGMEM il_combomap combomap[] = {
-  [C_HOME] = {.keys = {KC_LEFT, JP_HENK, 0}, .kana = SS_TAP(X_HOME)},
-  [C_PGUP] = {.keys = {KC_UP, KC_L, 0}, .kana = SS_TAP(X_PGUP)},
-  [C_PGDN] = {.keys = {KC_DOWN, KC_LEFT, 0}, .kana = SS_TAP(X_PGDN)},
-  [C_END]  = {.keys = {KC_RGHT, KC_DOWN, 0}, .kana = SS_TAP(X_END)},
+  // [C_HOME] = {.keys = {KC_LEFT, JP_HENK, 0}, .kana = SS_TAP(X_HOME)},
+  // [C_PGUP] = {.keys = {KC_UP, KC_L, 0}, .kana = SS_TAP(X_PGUP)},
+  // [C_PGDN] = {.keys = {KC_DOWN, KC_LEFT, 0}, .kana = SS_TAP(X_PGDN)},
+  // [C_END]  = {.keys = {KC_RGHT, KC_DOWN, 0}, .kana = SS_TAP(X_END)},
 };
 
 void process_combomap(uint8_t combo_index) {
@@ -17,8 +17,8 @@ void process_combomap(uint8_t combo_index) {
 }
 
 combo_t key_combos[COMBO_COUNT] = {
-  COMBO_ACTION(combomap[0].keys),
-  COMBO_ACTION(combomap[1].keys),
-  COMBO_ACTION(combomap[2].keys),
-  COMBO_ACTION(combomap[3].keys),
+  // COMBO_ACTION(combomap[0].keys),
+  // COMBO_ACTION(combomap[1].keys),
+  // COMBO_ACTION(combomap[2].keys),
+  // COMBO_ACTION(combomap[3].keys),
 };
